7f0157ed0e9cc18453b0bdb55d8e26720111aad4
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:02:00 +0530

    Create 4357-NumberOfElapsedSecondsBetweenTwoTimes

---
+++ 4357-NumberOfElapsedSecondsBetweenTwoTimes/4357-NumberOfElapsedSecondsBetweenTwoTimes.py
@@ -0,0 +1,9 @@
+# Last updated: 8/11/2026, 4:02:00 PM
+class Solution:
+    def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
+        h1,m1,s1=map(int,startTime.split(":"))
+        h2,m2,s2=map(int,endTime.split(":"))
+        start=h1*3600+m1*60+s1
+        end=h2*3600+m2*60+s2
+        return end - start
+        
