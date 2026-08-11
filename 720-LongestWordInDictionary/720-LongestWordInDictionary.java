1722b0567d04f84f366be55ba8ab3f83ea589033
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:07:19 +0530

    Create 720-LongestWordInDictionary

---
+++ 720-LongestWordInDictionary/720-LongestWordInDictionary.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 4:07:19 PM
+import java.util.*;
+
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+
+        Set<String> built = new HashSet<>();
+        String result = "";
+
+        for (String word : words) {
+            if (word.length() == 1 || built.contains(word.substring(0, word.length() - 1))) {
+                built.add(word);
+
+                if (word.length() > result.length()) {
+                    result = word;
+                }
+            }
+        }
+
+        return result;
+    }
+}
