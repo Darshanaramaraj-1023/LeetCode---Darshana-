0305b0a36949f1205b2dcc62b2e81492482493f9
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:09:42 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 4:09:42 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
