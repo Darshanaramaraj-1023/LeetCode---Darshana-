bfd13fb84a68ac5378725d474a30d902c5437305
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:10:10 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.py
@@ -0,0 +1,22 @@
+# Last updated: 8/11/2026, 4:10:10 PM
+class MyQueue:
+
+    def __init__(self):
+        self.input_stack = []
+        self.output_stack = []
+
+    def push(self, x: int) -> None:
+        self.input_stack.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output_stack.pop()
+
+    def peek(self) -> int:
+        if not self.output_stack:
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
+        return self.output_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.input_stack and not self.output_stack
