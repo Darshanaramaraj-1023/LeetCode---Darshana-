2b3f5bca7b1123e281442a3d99d559eafa71a68d
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:07:26 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,24 @@
+// Last updated: 8/11/2026, 4:07:27 PM
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
