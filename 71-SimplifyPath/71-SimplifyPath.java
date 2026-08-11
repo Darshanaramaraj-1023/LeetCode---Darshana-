6de6fe8825ba8ded8ac2f7f09b4ad1c6fee2dcae
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:12:35 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,35 @@
+// Last updated: 8/11/2026, 4:12:35 PM
+import java.util.*;
+class Solution {
+    public String simplifyPath(String path) {
+
+        String[] arr = path.split("/");
+        Stack<String> st = new Stack<>();
+
+        for (String ele : arr) {
+
+            if (ele.equals("") || ele.equals(".")) {
+                continue;
+            }
+
+            else if (ele.equals("..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            }
+
+            else {
+                st.push(ele);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String ele : st) {
+            sb.append("/");
+            sb.append(ele);
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
