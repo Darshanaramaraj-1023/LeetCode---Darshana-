e0597127448666390684488265c64e1c1ab5a55f
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:11:33 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,12 @@
+-- Last updated: 8/11/2026, 4:11:33 PM
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END
