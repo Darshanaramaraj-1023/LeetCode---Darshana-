4893b528015b1c03a5532c122e1e5b7552ee1fd1
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:10:37 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -0,0 +1,20 @@
+# Last updated: 8/11/2026, 4:10:37 PM
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+            if s[i] in s_to_t:
+                if s_to_t[s[i]] != t[i]:
+                    return False
+            else:
+                s_to_t[s[i]] = t[i]
+
+            if t[i] in t_to_s:
+                if t_to_s[t[i]] != s[i]:
+                    return False
+            else:
+                t_to_s[t[i]] = s[i]
+
+        return True
