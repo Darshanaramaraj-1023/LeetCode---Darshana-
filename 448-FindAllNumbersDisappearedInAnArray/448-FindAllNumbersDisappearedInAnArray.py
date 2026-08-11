3ade34cea6387835263c2780a7394611d4c60536
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:08:50 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.py
@@ -0,0 +1,17 @@
+# Last updated: 8/11/2026, 4:08:50 PM
+from typing import List
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+
+        result = []
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)
+
+        return result
