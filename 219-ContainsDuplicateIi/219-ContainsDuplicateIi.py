7ccc1ce02be2621413965e5e04e8a021b29cc106
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:10:23 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.py
@@ -0,0 +1,13 @@
+# Last updated: 8/11/2026, 4:10:23 PM
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        last_index = {}
+
+        for i, num in enumerate(nums):
+            if num in last_index and i - last_index[num] <= k:
+                return True
+            last_index[num] = i
+
+        return False
