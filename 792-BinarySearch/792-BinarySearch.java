a5881ed9fcca68b86f881e4650dc96a01d8194ab
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-11 16:07:04 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 4:07:04 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
