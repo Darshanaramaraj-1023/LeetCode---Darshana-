5c8334428930a56bb599caf6600f8b69072e379c
DARSHANA RAMARAJ <darshanaramaraj1023@gmail.com>
2026-08-22 09:19:55 +0530

    Create 1141-UserActivityforthePast30DaysI

---
+++ 1141-UserActivityforthePast30DaysI/version/python/1141-UserActivityforthePast30DaysI_20260822_091954.py
@@ -0,0 +1,18 @@
+# Last updated: 8/22/2026, 9:19:54 AM
+1class Solution:
+2    def sortedArrayToBST(self, nums):
+3        
+4        def build(left, right):
+5            if left > right:
+6                return None
+7            
+8            mid = (left + right) // 2
+9            
+10            root = TreeNode(nums[mid])
+11            
+12            root.left = build(left, mid - 1)
+13            root.right = build(mid + 1, right)
+14            
+15            return root
+16        
+17        return build(0, len(nums) - 1)
